e6c31e85def5b2d542a19d6f4a8519b6513aa6c0
Imoesi Rasheed Izuagbe <rasheedjay13@gmail.com>
2026-07-18 13:43:21 +0100

    gitignore

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,3 +1,3 @@
-export const CLOUDINARY_CLOUD_NAME = "zigfrhad";
-export const CLOUDINARY_UPLOAD_PRESET = "heedworldtest";
-export const CLOUDINARY_API_KEY = "275815824576983";
+export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
+export const CLOUDINARY_API_KEY = import.meta.env.VITE_CLOUDINARY_API_KEY;
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7r6uuqKd1mGtLa3UkFktQoELnr_evo_k",
-  authDomain: "heedworld-205e3.firebaseapp.com",
-  projectId: "heedworld-205e3",
-  storageBucket: "heedworld-205e3.firebasestorage.app",
-  messagingSenderId: "949893389317",
-  appId: "1:949893389317:web:8966e3083ac0382987d06c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
